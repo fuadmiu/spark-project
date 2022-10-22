788889d5686fa4863dc1afaf6b564908babf7bc6
Fuad <mohammad.fuad@miu.edu>
2022-10-21 21:30:50 -0500

    Initial commit

---
+++ src/main/scala/SparkMain.scala
@@ -0,0 +1,20 @@
+import Utils.{mean, variance}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SparkMain extends App {
+  val conf = new SparkConf().setAppName("Spark and SparkSql").setMaster("local")
+  val sc = new SparkContext(conf)
+
+  sc.setLogLevel("ERROR")
+
+  val filePath = "input/iris.csv"
+
+  val textFile = sc.textFile(filePath)
+  val titleRow = textFile.first()
+  val data = textFile.filter(row => row != titleRow) // Assuming csv will always have a title row
+
+  val population = data.map(line => line.split(",")).map(x => (x(5), x(4).toDouble))
+  //TODO: Display the population
+
+  val meanVariance = population.groupByKey().sortByKey().mapValues(u => (mean(u), variance(u)))
+}
---
+++ src/main/scala/Utils.scala
@@ -0,0 +1,11 @@
+object Utils {
+  def mean(values: Iterable[Double]): Double = values.sum / values.count(_ => true)
+
+  def variance(items: Iterable[Double]): Double = {
+    val mean = Utils.mean(items)
+    val sl = items.map(item => Math.pow(item - mean, 2))
+    sl.sum / (sl.size - 1)
+  }
+
+  // TODO: create util method for resample
+}
